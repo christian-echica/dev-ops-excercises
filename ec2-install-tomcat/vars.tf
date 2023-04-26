6bf3998f3638c2747c7650a4e55c39bcf70e512c
Christian Echica <echica.christian@gmail.com>
2023-04-25 19:42:59 -0700

    bom

--- ec2-install-tomcat/vars.tf
+++
@@ -1,5 +0,0 @@
-variable "instance_ip" {
-  description = "The public IP address of the EC2 instance"
-  type        = string
-  default     = "54.237.228.232"
-}
--- tfInfra_job_docker-git-tomcat/vars.tf
+++
@@ -1,5 +0,0 @@
-variable "instance_ip" {
-  description = "The public IP address of the EC2 instance"
-  type        = string
-  default     = "3.92.169.15"
-}
