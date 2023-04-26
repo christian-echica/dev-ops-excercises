bd6996f90ee01471324e86327c57cb5786d9f11c
Christian Echica <echica.christian@gmail.com>
2023-04-26 09:27:45 -0700

    remove tfvars

--- tfInfra_job_docker-git-tomcat/ec2_instance.tf
+++ tfInfra_job_docker-git-tomcat/ec2_instance.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  region     = "us-east-1"
+  profile    = "default"
+}
+
 resource "aws_instance" "ec2_instance" {
     ami = "${var.ami_id}"
     count = "${var.number_of_instances}"
--- tfInfra_job_docker-git-tomcat/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-access_key = "AKIAW635LUPZ4TIDOJWX"
-secret_key = "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"
