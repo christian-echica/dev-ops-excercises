c0c8cac6bf5667eef78782df69ae1bf81516e03b
Christian Echica <echica.christian@gmail.com>
2023-05-06 21:12:43 -0700

    Clean ec2-install-tomcat

--- ansible3-project/ansible3-project/managed.tf
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b9e16252683364bf2d1f061f185986bde228dce647d725dcc14524c0b82ca77d
-size 1646
--- ansible3-project/ansible3-project/provider.tf
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:356714eaed3ea403f8269dc56670b357e1a16d69a6f0b6dbcf52b078286132bc
-size 141
